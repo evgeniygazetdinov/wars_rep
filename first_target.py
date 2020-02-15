e949e622b59d6a0d93d57356edfddae395a8ecf2
evgeniynokalevich <zplacesound@gmail.com>
2020-02-15 06:39:45 +0300

    add some function

--- first_target.py
+++ first_target.py
@@ -1,12 +1,21 @@
 import os
-
+import psutil
 
 def find_aviliable_devices():
-    pass
+    all = psutil.disk_partitions()
+    devices = []
+    for i in range(len(all)):
+            devices.append(all[i][0])
+    return devices
 
 def write_devices_into_file(devices):
     #return path file
-    pass
+    name = 'file.txt'
+    f = open(name, "w+")
+    for device in devices:
+        f.write(str(device)+'\n')
+    f.close()
+    return os.path.abspath(name)
 
 def read_file_with_information():
     pass
@@ -31,5 +40,9 @@ def find_info_about_device():
 def find_information_about_devices():
     devices = find_aviliable_devices()
     file_with_information = write_devices_into_file(devices)
-    type, space, etc = find_info_about_device(file_with_information)
-    print(type, space, etc)
+#    type, space, etc = find_info_about_device(file_with_information)
+#    print(type, space, etc)
+
+
+if __name__ =="__main__":
+    find_information_about_devices()
