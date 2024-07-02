5b68af91483382dc6e4ad3a499262b1a5eb19ee4
ev <zplacesound@gmail.com>
2024-07-02 10:23:57 +0300

    refactor TODO add native contains

--- lib/volves.go
+++ lib/volves.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"fmt"
 	"slices"
 )
 
@@ -10,9 +9,7 @@ var volveList = []rune{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
 func RemoveAllVolves(someString string) string {
 	var result string
 	for _, b := range someString {
-		if slices.Contains(volveList, b) {
-			fmt.Println(string(b))
-		} else {
+		if !slices.Contains(volveList, b) {
 			result = result + string(b)
 		}
 	}
