56d6e372b99634ddfc1af432eaf9f39e6eb363f8
ev <zplacesound@gmail.com>
2024-07-11 10:21:28 +0300

    begin with leetcode

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"wars_rep/lib"
 )
 
 func main() {
 
 	// lib.HandlePushButton()
-	// fmt.Println(lib.SumOfSeq(1, 5, 1))
-	fmt.Println(lib.Divisor(4))
+	fmt.Println(lib.ReverseParentheses("(abcd)"))
 
 }
