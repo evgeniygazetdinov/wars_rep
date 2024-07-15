479dc81925a0f4c5a266e303b893e0da27633b70
ev <zplacesound@gmail.com>
2024-07-15 10:13:53 +0300

    try to make reverse string with custom split as recursive way

--- lib/reverseSubstring.go
+++ lib/reverseSubstring.go
@@ -1,8 +1,62 @@
 package lib
 
 import (
+	"fmt"
 	"slices"
 )
+
+func constantArray() []string {
+	return []string{"(", ")"}
+}
+
+func Reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
+}
+
+func mySplit(s string) []string {
+	arr := []string{}
+	word := ""
+	openString := false
+	end := false
+	beginSym := constantArray()[0]
+	endSym := constantArray()[1]
+	for _, w := range s {
+		isExeception := slices.Contains(constantArray(), string(w))
+		if string(w) == beginSym {
+			openString = true
+			end = false
+		}
+
+		if string(w) == endSym {
+			openString = false
+			end = true
+			arr = append(arr, word)
+			word = ""
+		}
+		if openString && !isExeception && !end {
+			word = word + string(w)
+		}
+
+	}
+	return arr
+}
+
+func myRecursionCall(recusionString string) string {
+	resultString := ""
+	for _, rString := range recusionString {
+		fmt.Println(rString)
+	}
+	return resultString
+}
+
+func mySplit3(s string) []string {
+	arr := []string{}
+
+	return arr
+}
 
 func split2(s string) string {
 	var res string
@@ -14,9 +68,9 @@ func split2(s string) string {
 			res = res + curWord
 		}
 	}
-	return res
+	return Reverse(res)
 }
 
-func ReverseParentheses(s string) string {
-	return s
+func ReverseParentheses(s string) {
+	fmt.Println(mySplit(s))
 }
--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"wars_rep/lib"
 )
 
 func main() {
 
-<<<<<<< HEAD
 	// lib.HandlePushButton()
-	fmt.Println(lib.ReverseParentheses("(abcd)"))
-=======
-	// fmt.Println(lib.SumOfSeq(1, 5, 1))
-	fmt.Println(lib.Divisor(4))
->>>>>>> 3fa6daea100cf45d5c4c4baf990e4aa9c1587761
+	lib.ReverseParentheses("(u(love)i)")
 
 }
