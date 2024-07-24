9d024dd4d74c4ffe1989c46a2582c76edac9a4a8
ev <zplacesound@gmail.com>
2024-07-24 09:45:56 +0300

    find biger common stuff inside arrayЭ

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"wars_rep/lib"
 )
 
 func main() {
 
-	lib.ReverseParentheses("(u(love)i)")
+	// lib.ReverseParentheses("(u(love)i)")
 
+	d := []string{"flower", "flow", "flight"}
+	lib.LongerCommonPrefix(d)
 
 }
